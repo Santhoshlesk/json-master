a5fe49a70829e430b07013a260dec3fd3db1280d
SANTHOSHKUMAR A <santhoshkumar_a@apollopharmacy.org>
2026-01-09 22:31:42 +0530

    dep optimized

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base:'/json-master/',
+  build: {
+    outDir: 'docs'
+  },
   server: {
     host: "::",
     port: 8080,
